8125a834b58faf12b7bc1827a20c1c610e45dce6
sghosh246 <sghosh246@gmail.com>
2018-04-13 12:58:17 +0530

    Add files via upload

---
+++ calcvolume.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 11:15:04 2018
+@author: souravg
+To do : Write a Python program to find the volume of a sphere with diameter 12 cm. Formula: V=4/3 * π * r^3
+"""
+PI = 3.14
+diameter = 12
+radius = diameter/2
+Volume = (4/3) * PI * radius ** 3
+print("The Volume of the Sphere is %.2f" %Volume)
